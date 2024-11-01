d65e476cda24018ad62e7d43207c4e38e2981687
Bharani77 <bharani007dharan@gmail.com>
2024-11-01 21:20:53 +0530

    Update galaxy_2.js

--- galaxy_2.js
+++ galaxy_2.js
@@ -473,8 +473,8 @@ async function imprison() {
                 { type: 'click', selector: '.dialog__close-button > img' },
                 { type: 'xpath', xpath: "//a[contains(.,'Exit')]" }
             ]},
-           // { action: 'sleep', ms: 450 },
-           // { action: 'click', selector: '.start__user__nick' }
+            { action: 'sleep', ms: 250 },
+            { action: 'click', selector: '.start__user__nick' }
 		{ action: 'reloadPage' }
         ];
 
